a71056442b176269edf88d82423a26c1ce87ec79
Ivan Jovkovic <30907760+jokac88@users.noreply.github.com>
2019-03-05 15:36:39 +0100

    add component, fullpage.js

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="fixed">
+    <div class="row align-items-center">
+      <div class="col-md-2"></div>
+      <div class="col-md-10 text-right">
+        <ul>
+          <li>
+            <a href="javascript:void(0)">Home</a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">About us</a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">Menu</a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">This week</a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">Gallery</a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,53 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" href="{{ asset('css/app.css?ver=' . time()) }}">
-    </head>
-    <body>
-    	<div id="app">
-    		<example-component></example-component>
-    	</div>
-        <script src="{{ asset('js/app.js?ver=' . time()) }}"></script>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ asset('css/fullpage.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css?ver=' . time()) }}">
+</head>
+
+<body>
+    <div id="app">
+        <header-component></header-component>
+        <div id="fullpage">
+            <div class="section">
+                Ivan
+            </div>
+            <div class="section">
+                a
+            </div>
+            <div class="section">
+            </div>
+            <div class="section">
+            </div>
+            <div class="section">
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-ui.js') }}"></script>
+    <script src="{{ asset('js/scrolloverflow.min.js') }}"></script>
+    <script src="{{ asset('js/fullpage.min.js') }}"></script>
+    <script src="{{ asset('js/fullpage.extensions.min.js') }}"></script>
+    <script src="{{ asset('js/app.js?ver=' . time()) }}"></script>
+    <script>
+        new fullpage('#fullpage', {
+            //options here
+            // licenseKey: 'xxxxxxxx-xxxxxxxx-xxxxxxxx-xxxxxxxx',
+            autoScrolling: true,
+            scrollHorizontally: true,
+            scrollingSpeed: 1000,
+            // scrollBar: true,
+            scrollOverflow: true,
+            css3: true
+        });
+
+        //methods
+        fullpage_api.setAllowScrolling(false);
+    </script>
+</body>
+
 </html>
