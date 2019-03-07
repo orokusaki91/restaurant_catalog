1387c49beaba23f08bf82f84ea48381e5ec82582
Ivan Jovkovic <30907760+jokac88@users.noreply.github.com>
2019-03-07 18:48:04 +0100

    about us

--- public/js/app.js
+++ public/js/app.js
@@ -2724,6 +2724,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
@@ -38475,21 +38476,27 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "section", attrs: { id: "aboutUs" } }, [
-    _c("div", { staticClass: "container-wrap" }, [
-      _c("div", { staticClass: "container-wrap-background" }, [
-        _c("div", { staticClass: "container p-5" }, [
-          _c("div", { staticClass: "row align-items-center" }, [
-            _vm._m(0),
+  return _c(
+    "div",
+    { staticClass: "section active", attrs: { id: "aboutUs" } },
+    [
+      _c("div", { staticClass: "container-wrap" }, [
+        _c("div", { staticClass: "container-wrap-background" }, [
+          _c("div", { staticClass: "container p-5" }, [
+            _c("div", { staticClass: "row align-items-center" }, [
+              _vm._m(0),
+              _vm._v(" "),
+              _c("div", { staticClass: "col-md-3" }, [
+                _c("img", { attrs: { src: _vm.food, alt: _vm.food } })
+              ])
+            ]),
             _vm._v(" "),
-            _c("div", { staticClass: "col-md-3" }, [
-              _c("img", { attrs: { src: _vm.food, alt: _vm.food } })
-            ])
+            _c("hr")
           ])
         ])
       ])
-    ])
-  ])
+    ]
+  )
 }
 var staticRenderFns = [
   function() {
@@ -38515,13 +38522,13 @@ var staticRenderFns = [
         )
       ]),
       _vm._v(" "),
-      _c("button", { staticClass: "btn" }, [
+      _c("button", { staticClass: "btn btn-readMore" }, [
         _vm._v(
           "\n                            Read more\n                        "
         )
       ]),
       _vm._v(" "),
-      _c("button", { staticClass: "btn" }, [
+      _c("button", { staticClass: "btn btn-book" }, [
         _vm._v(
           "\n                            Book a table\n                        "
         )
--- resources/js/components/AboutUs.vue
+++ resources/js/components/AboutUs.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="aboutUs" class="section">
+    <div id="aboutUs" class="section active">
         <div class="container-wrap">
             <div class="container-wrap-background">
                 <div class="container p-5">
@@ -22,10 +22,10 @@
                                 ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
                                 reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                             </p>
-                            <button class="btn">
+                            <button class="btn btn-readMore">
                                 Read more
                             </button>
-                            <button class="btn">
+                            <button class="btn btn-book">
                                 Book a table
                             </button>
                         </div>
@@ -33,6 +33,7 @@
                             <img :src="food" :alt="food" />
                         </div>
                     </div>
+                    <hr />
                 </div>
             </div>
         </div>
